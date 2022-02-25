1c6a74726d33be723e59af135c27bc840858f6a4
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-02-25 08:46:27 +0100

    add: loops task 1 solution

---
+++ loop/task-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a simple script which will print the numbers 1 - 10 
+# (each on a separate line) and whether they are even or odd.
+
+for var in {1..10}; do
+    echo $var
+done
